da9483612621c473659c3ee5e3e5e0f605d9f676
Patrick-van-Halm <pvanhalm00@gmail.com>
2021-03-25 11:01:42 +0100

    Remove use of envHandler in hasher

--- internal/hasher/hasher.go
+++ internal/hasher/hasher.go
@@ -2,20 +2,20 @@ package hasher
 
 import (
 	"github.com/speps/go-hashids"
-	"github.com/Patrick-van-Halm/nuggets_books_blog-api/internal/env"
 	"log"
+	"os"
 	"strconv"
 )
 
 var idHasher *hashids.HashID
 func init() {
-	length, err := strconv.Atoi(env.GetEnvVar("ID_HASHER_MINLENGTH"))
+	length, err := strconv.Atoi(os.Getenv("ID_HASHER_MINLENGTH"))
 	if err != nil {
 		log.Fatalln(err)
 		return
 	}
 	hd := hashids.NewData()
-	hd.Salt = env.GetEnvVar("ID_HASHER_SALT")
+	hd.Salt = os.Getenv("ID_HASHER_SALT")
 	hd.MinLength = length
 	idHasher, err = hashids.NewWithData(hd)
 	if err != nil {
