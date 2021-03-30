f4272fea15fd0a6a94bb82cd2caac3737c312cf9
Patrick-van-Halm <pvanhalm00@gmail.com>
2021-03-30 11:40:57 +0200

    Add New and GetAll

--- internal/classes/Author.go
+++ internal/classes/Author.go
@@ -21,3 +21,41 @@ func (a *Author) Get(db *sql.DB) error {
 	}
 	return nil
 }
+
+func (a *Author) New(db *sql.DB) error {
+	if a.Name == "" {
+		return errors.New("name cannot be empty")
+	}
+
+	id, err := gonanoid.New(21)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec("INSERT INTO authors (id, name) VALUES ($1, $2)", id, a.Name)
+	if err == nil {
+		a.Id = id
+	}
+	return err
+}
+
+func GetAllAuthors(db *sql.DB) ([]*Author, error) {
+	authors := make([]*Author, 0)
+
+	rows, err := db.Query(`SELECT id, name FROM authors`)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var a Author
+		err := rows.Scan(&a.Id, &a.Name)
+		if err != nil {
+			return nil, err
+		}
+
+		authors = append(authors, &a)
+	}
+
+	return authors, nil
+}
